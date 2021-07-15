25308e05bc8abaa825dc536cf039d056c8afb15c
Sudipta acharjee <sudiptoacharjee4@gmail.com>
2021-07-15 14:11:29 +0600

    data transfer successfully

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require('express');
+// const express = require('express');
+var express = require('express')
+var cors = require('cors')
+var app = express()
+const port = 5000;
+
+app.use(cors())
 
 const MongoClient = require('mongodb').MongoClient;
 const password = 'pTZK5hQtvF3D2Dxd';
 const uri = "mongodb+srv://internUser:pTZK5hQtvF3D2Dxd@cluster0.f3vnz.mongodb.net/interndb?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const app = express();
+// const app = express();
 
 app.get('/', (req, res) => {
     res.send('hello I am working')
@@ -13,12 +19,15 @@ app.get('/', (req, res) => {
 
 client.connect(err => {
     const collection = client.db("interndb").collection("infos");
-    const user = { name: "sudipta", email: "sudipto@gmail.com", phone: "018552225"};
-    collection.insertOne(user)
-        .then(result => {
-            console.log("One user added");
-        })
+    app.post('/addUser', (req, res) => {
+        const user = req.body;
+        console.log(user);
+        // collection.insertOne(user)
+        // .then(result => {
+        //     console.log(result);
+        // })
+    })
 });
 
 
-app.listen(3000);
+app.listen(port);
